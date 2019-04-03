e07c5cdf49464ef2550c4a78535904622a7979e4
Pedro <pedro.belmonte@ing.uchile.cl>
2019-04-02 22:19:48 -0300

    algunos modelos

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+#class Usuario():
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Empleo(models.Model):
+    tipo = models.CharField(max_length=100)
+
+class Empresa(models.Model):
+    #Pyp?
+    #logo = models.ImageField(upload_to=, null=True)
+    ofertas = models.TextField() # supongo que vamos a guardar el json como string?
+    descripcion = models.CharField(max_length=300)
+    web = models.URLField()
+    #rut =
+    nombre = models.CharField(max_length=100)
+    encuesta = models.URLField()
+
+class ImagenEmpresa(models.Model):
+    #imagen = models.ImageField(upload_to=, null=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
