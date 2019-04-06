6b628156533725dce9ba76dc0fd6c5ad13d4f1f8
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2019-04-05 22:26:31 -0300

    configuración postresql

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'plataforma.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'redesbeauchef',
+        'USER': 'redesbeauchef',
+        'PASSWORD': '6M#HadA%RbH$kTxv',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
