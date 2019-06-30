39044d27cfeca5a7acbdee2a8abd2fceabe271f8
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2019-06-29 21:15:40 -0400

    una sola fila funcionando

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, User
 
 
 class Carrera(models.Model):
-    plan = models.CharField(max_length=150)
+    plan = models.CharField(max_length=150, unique=True)
 
     def __str__(self):
         return self.plan
--- requerimientos/views.py
+++ requerimientos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from main_app.models import Carrera
 
 # Create your views here.
 class Requerimientos(TemplateView):
@@ -9,4 +10,5 @@ class Requerimientos(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         context['title'] = 'requerimientos'
+        context['carreras'] = Carrera.objects.all().order_by('plan')
         return context
